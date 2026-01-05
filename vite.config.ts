06b82f6dd8a45e6bbdd46630817a96d7aa7710f3
Aster <github@aster.simplelogin.com>
2026-01-05 08:57:54 -0500

    Added railway as allowedHost

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
   plugins: [glsl()],
   optimizeDeps: {
     exclude: ['@babylonjs/havok']
+  },
+  preview: {
+    host: true,
+    allowedHosts: ['paint-production-435f.up.railway.app']
   }
 });
